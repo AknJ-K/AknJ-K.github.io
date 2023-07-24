7cd1c2cf1d88f8cc26ff8558482722f8ceafedd1
AknJ-K <kaamiltull@gmail.com>
2023-07-24 07:31:09 -0400

    .

--- src/home.js
+++ src/home.js
@@ -2,23 +2,6 @@ document.addEventListener('DOMContentLoaded', function () {
     const GoButton = document.getElementById('go-button');
     const Chooser = document.getElementById("webpage-chooser");
     GoButton.onclick = function () {
-        switch (Chooser.value) {
-            case "games":
-                window.location.href = "./games";
-                break;
-
-            case "music":
-                window.location.href = "./music";
-                break;
-
-            case "misc":
-                window.location.href = "./misc";
-                break;
-
-            default:
-                console.warn("yeah this isn't supposed to happen huh\nhome.js uhhhh idk what else");
-                window.location.href = "./404";
-                break;
-        }
+        window.location.href = "./" + Chooser.value + ".html";
     }
 });
